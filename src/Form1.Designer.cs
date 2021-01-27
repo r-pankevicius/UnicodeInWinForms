0d07ad7f79a8b7bba566fb93038e5095e75be7b9
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2021-01-27 21:32:58 +0200

    How CompatibleTextRendering affects displaying of unicode chars

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace UnicodeInWinForms
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label4Label = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label4LinkLabel = new System.Windows.Forms.Label();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label4Label
+			// 
+			this.label4Label.AutoSize = true;
+			this.label4Label.Location = new System.Drawing.Point(12, 9);
+			this.label4Label.Name = "label4Label";
+			this.label4Label.Size = new System.Drawing.Size(513, 17);
+			this.label4Label.TabIndex = 0;
+			this.label4Label.Text = "Below you see WinForms Label control. Use CompatibleTextRendering is FALSE";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 39);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(72, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "🔒 Locked";
+			// 
+			// label4LinkLabel
+			// 
+			this.label4LinkLabel.AutoSize = true;
+			this.label4LinkLabel.Location = new System.Drawing.Point(12, 183);
+			this.label4LinkLabel.Name = "label4LinkLabel";
+			this.label4LinkLabel.Size = new System.Drawing.Size(855, 17);
+			this.label4LinkLabel.TabIndex = 0;
+			this.label4LinkLabel.Text = "Below you see WinForms LinkLabel control. It\'s text is the same as text in Label " +
+    "control above. Use CompatibleTextRendering is FALSE";
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoSize = true;
+			this.linkLabel1.Location = new System.Drawing.Point(12, 217);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(72, 17);
+			this.linkLabel1.TabIndex = 2;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "🔒 Locked";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 259);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(850, 17);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Below you see WinForms LinkLabel control. It\'s text is the same as text in Label " +
+    "control above. Use CompatibleTextRendering is TRUE";
+			// 
+			// linkLabel2
+			// 
+			this.linkLabel2.AutoSize = true;
+			this.linkLabel2.Location = new System.Drawing.Point(12, 293);
+			this.linkLabel2.Name = "linkLabel2";
+			this.linkLabel2.Size = new System.Drawing.Size(55, 20);
+			this.linkLabel2.TabIndex = 2;
+			this.linkLabel2.TabStop = true;
+			this.linkLabel2.Text = "🔒 Locked";
+			this.linkLabel2.UseCompatibleTextRendering = true;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 76);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(508, 17);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Below you see WinForms Label control. Use CompatibleTextRendering is TRUE";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 106);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(55, 20);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "🔒 Locked";
+			this.label4.UseCompatibleTextRendering = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1040, 363);
+			this.Controls.Add(this.linkLabel2);
+			this.Controls.Add(this.linkLabel1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label4LinkLabel);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label4Label);
+			this.Name = "Form1";
+			this.Text = "https://github.com/r-pankevicius/UnicodeInWinForms";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label4Label;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label4LinkLabel;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.LinkLabel linkLabel2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
